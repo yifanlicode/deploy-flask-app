578fe774abf1554a4ea6b7db1d631857a1703320
yifanli <yifanlicode@gmail.com>
2023-08-07 18:17:23 +0000

    first commit

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,24 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from PIL import Image
+from pytube import YouTube
 
-# Always use relative import for custom module
-from .package.module import MODULE_VALUE
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return (
-        "Try /hello/Chris for parameterized Flask route.\n"
-        "Try /module for module import guidance"
-    )
+    return render_template('index.html')
 
-@app.route("/hello/<name>", methods=['GET'])
-def hello(name: str):
-    return f"hello {name}"
+@app.route('/convert', methods=['POST'])
+def convert():
+    image = request.files['image']
+    output_format = request.form['output_format']
 
-@app.route("/module")
-def module():
-    return f"loaded from FlaskApp.package.module = {MODULE_VALUE}"
+    try:
+        # Open the image
+        im = Image.open(image)
+
+        # Convert to RGB if necessary
+        if im.mode != 'RGB':
+            im = im.convert('RGB')
+
+        # Save the converted image to a file
+        converted_image_path = 'converted_image.' + output_format
+        im.save(converted_image_path, format=output_format)
+
+        # Return the message with the converted image
+        return render_template('index.html', message='Image successfully converted!')
+    except Exception as e:
+        return render_template('index.html', message=f'Error: {str(e)}')
+
+@app.route('/download', methods=['GET', 'POST'])
+def download():
+    if request.method == 'POST':
+        video_url = request.form['video_url']
+        try:
+            yt = YouTube(video_url)
+            stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+            stream.download(output_path='downloads')
+            return 'Video downloaded successfully!'
+        except Exception as e:
+            return f'Error: {str(e)}'
+    else:
+        return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
